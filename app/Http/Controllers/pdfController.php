1db7fd560de57796ba14df4fe0af1b8c017eff5e
https://vincentbattez.fr <vincent.battez.pro@gmail.com>
2018-05-13 22:15:22 +0200

    first commit

---
+++ app/Http/Controllers/pdfController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PDF;
+
+class pdfController extends Controller
+{
+    public function devis() {
+        $data = ['toto'];
+        $pdf  = PDF::loadView('pdf.devis', $data);
+        // return view('pdf.devis');
+        
+        // dd($pdf);
+        return $pdf->download('devis1.pdf');
+    }
+}
---
+++ resources/views/pdf/devis.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+<body>
+  <h2>Titre</h2>
+  <h1>DEVIS</h1>
+  <table>
+    <tr>
+      <td>Description</td>
+      <td>Heures</td>
+      <td>Date de livraison</td>
+      <td>Montant</td>
+    </tr>
+    <tr>
+      <td>
+        <ul>
+          <li>Conception des Wireframes
+            <ul>
+              <li>Réflexion sur l’ergonomie du site web (UX)</li>
+              <li>Schéma global du site web (sans design)</li>
+            </ul>
+          </li>
+        </ul>
+      </td>
+      <td>20h</td>
+      <td>07/06/2018 (7 jours)</td>
+      <td>300,00 €</td>
+    </tr>
+  </table>
+</body>
+</html>
